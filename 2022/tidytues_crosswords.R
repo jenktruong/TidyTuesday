456711723afb9ca9a3b13b43c37468e264256f9a
jenjentro <jktruong@bren.ucsb.edu>
2022-04-23 17:24:43 -0700

    Started R script

---
+++ 2022/tidytues_crosswords.R
@@ -0,0 +1,15 @@
+#####
+# TidyTuesday 2022 W16 - 2022-04-19
+# Crosswords
+# Jennifer Truong
+#####
+
+# Load packages ----
+library(tidyverse)
+library(tidytuesdayR)
+library(janitor)
+library(here)
+library(showtext)
+
+# Load dataset ----
+tuesdata <- tidytuesdayR::tt_load('2022-04-19')
