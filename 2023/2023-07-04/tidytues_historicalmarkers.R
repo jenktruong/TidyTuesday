b155e22a5c4e5b344e01f17b11e7110cd182f59f
jenktruong <jktruong@ucsb.edu>
2023-07-06 16:19:10 -0700

    Added new R script for this week's TidyTuesday

---
+++ 2023/2023-07-04/tidytues_historicalmarkers.R
@@ -0,0 +1,20 @@
+#####
+# TidyTuesday 2023 W27 - 2023-07-06
+# Historical Markers
+# Jennifer Truong
+#####
+
+# Load packages ----
+library(tidyverse)
+library(tidytuesdayR)
+library(janitor)
+library(here)
+library(showtext)
+
+# Load dataset ----
+
+# Data wrangling ----
+
+# Load fonts ----
+
+# Show plot ----
