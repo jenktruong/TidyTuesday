ebbc28f211e0b6a7f04218422bbb95ff939c01a0
jenktruong <jktruong@ucsb.edu>
2023-07-06 16:39:28 -0700

    Added basic code for plot

--- 2023/2023-07-04/tidytues_historicalmarkers.R
+++ 2023/2023-07-04/tidytues_historicalmarkers.R
@@ -12,9 +12,25 @@ library(here)
 library(showtext)
 
 # Load dataset ----
+tuesdata <- tidytuesdayR::tt_load(2023, week = 27)
+
+historical_markers <- tuesdata$`historical_markers`
+no_markers <- tuesdata$`no_markers`
 
 # Data wrangling ----
+ 
+hist_markers_df <- historical_markers %>% 
+  select(marker_id, year_erected, city_or_town, state_or_prov) %>% 
+  group_by(state_or_prov, year_erected) %>% 
+  count()
 
 # Load fonts ----
+# font_add_google("actualfont", "nickname")
+# showtext_auto()
 
 # Show plot ----
+ggplot(hist_markers_df,
+       aes(x = year_erected, y = n)) +
+  geom_line(aes(color = state_or_prov))
+
+# Save plot ----
