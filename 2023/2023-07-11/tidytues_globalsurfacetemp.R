b06a461fe5f8422c74afc2159ebc27b4d622ba6c
jenktruong <jktruong@ucsb.edu>
2023-07-30 19:18:09 -0700

    Started set up of plot, additional data wrangling

--- 2023/2023-07-11/tidytues_globalsurfacetemp.R
+++ 2023/2023-07-11/tidytues_globalsurfacetemp.R
@@ -41,9 +41,80 @@ zonann_temps <- tuesdata$zonann_temps
 # Sep-Oct-Nov (SON) = N.H. meteorological autumn
 
 nh_season_temp <- nh_temps %>% 
-  clean_names() %>% # Change column names to lower case
-  select(year, djf, mam, jja, son) # Only get year and seasonal columns only
+  clean_names() %>% 
+  select(year, djf, mam, jja, son) %>% 
+  # pivot three-month column names to one row
+  pivot_longer(
+    cols = djf:son,
+    names_to = "season",
+    values_to = "temp"
+  ) %>% 
+  # Create new columns with season name
+  mutate(
+    season_name = case_when(
+      season == "djf" ~ "Winter",
+      season == "mam" ~ "Spring",
+      season == "jja" ~ "Summer",
+      season == "son" ~ "Fall"
+    )
+  )
 
 # Add fonts ----
 font_add_google("Mulish", "mulish")
 showtext_auto()
+
+# Create plot ----
+
+# Color scheme
+background_color = '#000000'
+font_color = "#FFFFFF"
+
+# Background
+canvas <- grid::rectGrob(
+  x = 0, y = 0,
+  width = 16, height = 9,
+  gp = grid::gpar(fill = background_color, alpha = 1, col = background_color)
+)
+
+# Margins
+plot_margin <- 0.05
+
+# Main plot
+plot_nh_temp <- ggplot(nh_season_temp,
+                       aes(x = year, y = temp)) +
+  geom_waffle(color = "white", size = 1.125, n_rows = 10, 
+              make_proportional = TRUE,
+              stat = "identity", na.rm = TRUE) +
+  facet_wrap(~factor(season_name, 
+                     levels = c("Winter", "Spring", "Summer", "Fall"))) +
+  coord_equal(clip = "off") + 
+  scale_x_discrete(expand = c(0,0)) +
+  scale_y_discrete(expand = c(0,0)) +
+  scale_fill_manual(values = c('#36161a', "#b54a56","#e9c9cc"), 
+                    breaks = c('high', 'moderate', 'low'), 
+                    labels = c("High", "Moderate", "Low"),
+                    name = NULL) +
+  labs(title = "Northern Hemisphere Surface Temperatures",
+       subtitle = "Looking at global surface temperatures in the Northern Hemishere over time by year",
+       x = "Year",
+       y = "Deviations from 1951-1980 means") #+
+  # theme() +
+  # guides(fill = guide_legend(title.position = "top"))
+
+# Combine all plot elements and add annotation 
+#ggdraw(ylim = c(0,1), # 0-1 bounds make it easy to place viz items on canvas
+#       xlim = c(0,1)) +
+  # a background
+#  draw_grob(canvas,
+#            x = 0, y = 1,
+#            height = 12, width = 12,
+#            hjust = 0, vjust = 1) +
+  # the main plot
+#  draw_plot(plot_fe,
+#            x = plot_margin - 0.1,
+#            y = plot_margin,
+#            height = 0.94,
+#            width = 0.94) 
+
+# Save figure ----
+# ggsave("geomcurve.png", width = 12, height = 12, dpi = 300, bg = "white")
